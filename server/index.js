74d9d73813bb3bef0ea592c23e9a8bd4f932c57e
silva.ext rai <rai.silva.ext@sodexo.com>
2022-03-14 19:52:25 -0300

    Added config.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
+import config from './config.js';
 import server from './server.js';
 import {logger} from './util.js'
 
-server.listen(3000).on('listening', () => logger.info("rodando"))
+server.listen(config.port).on('listening', () => logger.info(`running at ${config.port}`))
