a109c4d5f788a536b7067e047aec589da3165dc0
silva.ext rai <rai.silva.ext@sodexo.com>
2022-03-14 20:34:10 -0300

    Add routes.

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,40 @@
+import config from './config.js'
+import { Controller } from './controller.js'
+import {logger} from './util.js'
+
+const {location, pages:{homeHTML, controllerHTML}} = config
+const controller = new Controller
+async function routes(request, response){
+    const {method, url} = request
+
+    if(method === 'GET' && url === '/') {
+        response.writeHead(302, {
+            'Location': location.home
+        })
+        return response.end()
+    }
+
+    if(method === 'GET' && url === '/home') {
+        const {stream}=await controller.getFileStream(homeHTML)
+        return stream.pipe(response)
+    }
+
+    if(method === 'GET' && url === '/controller') {
+        const {stream}=await controller.getFileStream(controllerHTML)
+        return stream.pipe(response)
+    }
+
+    if(method === 'GET'){
+        const {stream, type} = await controller.getFileStream(url)
+
+        return stream.pipe(response);
+    }
+    response.writeHead(404)
+    return response.end()
+}
+
+
 export function handler(request, response){
-    return response.end('hello')
+    return routes(request, response)
+    .catch(error => logger.error(`Erro: ${error.stack}`))
 }
